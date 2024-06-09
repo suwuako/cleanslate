1f8e8a77b35c825a0e5b2d915b594c32a617c87b
Vincent <allLogarithmsEqual@protonmail.com>
2024-06-08 22:24:34 -0400

    Lay groundwork for automated deletion bot
    Provide a place to put our discord bot code and the credentials for
    the bot when we move to testing. These are the most fundamental files
    for our goals. (It may be the case that no other files are necessary!)
    
    The structure of this code was inspired by our Nextcloud DataBot. You
    can license it however you want, though you might want to bear that in
    mind. But we own the copyright on that, so it shouldn't be a problem.

---
+++ launch.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+    Copyright (c) 2024 suwa, chocorho
+"""
+
+import datetime
+import discord
+from discord.ext import commands
+import secret
+
+"""
+    A discord bot to be used by Moderators to manage messages in large numbers.
+"""
+class CleanSlateBot:
+  def __init__(self, spreadsheet_obj):
+    @bot.event
+    async def on_ready():
+      print(f'We have logged in as {bot.user}')
+    
+    @bot.event
+    async def on_disconnect():
+      print("Bot has disconnected.")
+    
+    self.spreadsheet = spreadsheet_obj
+  
+  def commands(self):
+    @bot.command()
+    async def ping(message):
+      await message.channel.send('Pong!')
+    
+    @bot.command()
+    async def redact(message, target):
+      if (None == target):
+        await message.channel.send('target not specified. Try again.')
+      else:
+        end_date = datetime.datetime(2021, 5, 1)
+        async for historical_message in message.channel.history(limit=None, before=end_date):
+          if (target == message.author.id):
+            print(f"now deleting the next message, from {message.created_at}")
+            message.delete()
+            time.sleep(5)
+  
+  def run(self):
+    self.commands()
+    bot.run(secret.credentials.bottoken)
+
+
+if __name__ == '__main__':
+  CleanSlateBot.run()
+
