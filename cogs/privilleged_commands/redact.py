a1f7545d192aee37dba4d744bbdf94dbc69d13c1
suwa <suwuako@protonmail.com>
2024-06-27 01:47:36 +1000

    start migrating redact command to cogs

---
+++ cogs/privilleged_commands/redact.py
@@ -0,0 +1,40 @@
+import secret
+
+import datetime
+import time
+
+import asyncio
+import discord
+from discord.ext import commands
+
+
+class Redact(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(aliases=["purge"])
+    async def redact(self, message, target_str):
+        
+        target = int(target_str)
+
+        if (None == target):
+            await message.channel.send('target not specified. Try again.')
+            return
+
+        if (message.author.id not in secret.credentials.privileged_users):
+            await message.channel.send("no perms, byebye")
+            return
+        
+        await message.channel.send(f"Deleting messages for {target}...")
+        
+        #end_date = datetime.datetime(2021, 5, 1)
+        async for historical_message in message.channel.history(limit=None):
+            print(f"{historical_message.content}")
+            if (target == historical_message.author.id):
+                print(f"now deleting the next message, from {historical_message.created_at}")
+                historical_message.delete()
+                asyncio.sleep(5)
+
+async def setup(bot):
+    await bot.add_cog(Redact(bot))
